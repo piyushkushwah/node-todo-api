10334ad4fe8fae13207307a62d4e3c5c6fb5289b
piyush kushwah <kushwahpiyush4@gmail.com>
2018-06-22 14:52:21 +0530

    Ok!

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise =global.Promise;
  MONGOLAB_URI="mongodb://piyush846:piyush@846@ds018508.mlab.com:18508/todo_api"
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp');
 
 
 module.exports={mongoose};
